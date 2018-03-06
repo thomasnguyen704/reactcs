1243ad31b70e9194d94749b3d23416dd4f0f3930
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro.local>
2018-03-06 09:12:41 -0600

    new api endpoints

--- server/api_endpoints.js
+++ server/api_endpoints.js
@@ -9,26 +9,25 @@ const knex = require('knex')({
 
 app.use(bodyParser)
 
-const getProjects = ()=> {
-    return knex.select('id', 'project', 'lead', 'status', 'remediation').from('projects')
-}
-const getSkills = ()=> {
-    return knex.select('id', 'skill').from('skills')
-}
-const getSurveys = ()=> {
-    return knex.select('id', 'skill_id', 'user').from('surveys')
-}
-const getProjectAssociates = ()=> {
-    return knex.select('project_id', 'associate').from('project_associates')
-}
-const getProjectSkills = ()=> {
-    return knex.select('skill_id', 'project_id').from('project_skills')
-}
+/*
+const getProjects = ()=> { return knex.select('id', 'project', 'lead', 'status', 'remediation').from('projects') }
+const getSkills = ()=> { return knex.select('id', 'skill').from('skills') }
+const getSurveys = ()=> { return knex.select('id', 'skill_id', 'user').from('surveys') }
+const getProjectAssociates = ()=> { return knex.select('project_id', 'associate').from('project_associates') }
+const getProjectSkills = ()=> { return knex.select('skill_id', 'project_id').from('project_skills') }
+*/
 
-/* table: get all projects by project id */
+// table: get all projects
+app.get('/projects', (req, res)=> {
+    return knex.select('id', 'project', 'lead', 'status', 'remediation')
+        .from('projects')
+        .then(results=> {res.send(results)})
+})
+
+// table: get all projects by project id
+// exiting form: get a project by project id this will include assigned assoc
 app.get('/projects/:id', (req, res)=> {
     const projectId = req.params.id
-    
     const results = Promise.all([
         knex.select('project', 'lead', 'status', 'remediation')
             .from('projects')
@@ -43,14 +42,11 @@ app.get('/projects/:id', (req, res)=> {
             .where('project_associates.project_id', projectId)
     ]).then(results=> {
         // results is an array, first item is the project, second is skill, third is associates
-        if(results[0].length === 0){
+        if(results[0].length===0){
             throw new Error ('No projects for that ID')
-        }
-        else {
-            res.send({
-                ...results[0][0],
-                skills: results[1],
-                associates: results[2]
+        } else {
+            res.send({ 
+                ...results[0][0], skills: results[1], associates: results[2]
             })
         }
     }).catch(error=>{
@@ -58,42 +54,47 @@ app.get('/projects/:id', (req, res)=> {
     })
 })
 
-
-app.get('/projects', (req, res)=> {
-    return knex.select('id', 'project', 'lead', 'status', 'remediation')
-        .from('projects').then(
-        results=> {
-            res.send(results)
-        }
-    )
-})
-
-
-/*
-    exiting form: get a project by project id
-    this will include assigned assoc
-*/
-
 /*
     new form: post a project form
     this will create a new project id (auto inc)
     this will include assigned assoc
     this will include assigned skills
 */
+app.post('/projects/:id', (req, res)=> {
+    req.body()
+})
 
-/*
-    get skills from skills table
-*/
+// get skills from skills table
+app.get('/skills', (req, res)=> {
+    return knex.select('id', 'skill')
+    .from('skills')
+    .then(results=> {res.send(results)})
+})
 
-/*
-    get lead from users table
-    get associates from users table
-*/
+// get lead from users table
+app.get('/leads', (req, res)=> {
+    return knex.select('lead')
+    .from('projects')
+    .then(results=> {res.send(results)})
+})
 
-/*
-    get an associate from user/survey table
-    get skills by associate from surveys table
-*/
+// get associates from users table
+app.get('/users', (req,res)=>{
+    return knex.select('user')
+    .from('users')
+    .then(results=> {res.send(results)})
+})
+
+// get an associate from user/survey table
+app.get('/surveys:user', (req, res)=> {
+    return knex.select('id', 'skill_id', 'user')
+    .from('surveys')
+    .where('user', req.params.user)
+    .then(results=> {res.send(results)})
+})
+
+// get skills by associate from surveys table
+app.get('')
 
 /*
     post skill by associate to the survey table
