93c5e393cd0bd1496dfc1a53330d528f04bb5038
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro.local>
2018-04-15 10:28:03 -0500

    url changed for testing

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
-export const url = process.env.production? '159.65.189.161:3000' : ''
+// export const url = process.env.production? '159.65.189.161:3000' : ''
+export const url = '159.65.189.161:3000' || ''
