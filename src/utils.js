23e866c236a33a101062192fa9eadedc617a5073
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro.local>
2018-04-16 16:17:08 -0500

    url

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,1 @@
-// export const url = process.env.production? '159.65.189.161:3000' : ''
-export const url = '159.65.189.161:3000' || ''
+export const url = process.env.production? 'http://159.65.189.161:3000' : 'http://localhost:3000'
