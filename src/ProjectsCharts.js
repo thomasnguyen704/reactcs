aa74536f06f438e8f58d830cafde562b5f2b8f19
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro.local>
2018-04-17 14:01:47 -0500

    styles

--- src/ProjectsCharts.js
+++ src/ProjectsCharts.js
@@ -151,7 +151,7 @@ class ProjectsCharts extends React.Component {
                     </Col>
                 </Row>
 
-                <p style={sectionHeader}>Some Header</p>
+                <p style={sectionHeader}>Skill Reqiurements Overview</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <Divider />
                 <Row>
